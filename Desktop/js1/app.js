f7bd436e21e9a91cc3841905d2a495a3ad8d6146
高杉　蓮 <sugi@takasugirennoMacBook-Pro.local>
2021-05-23 18:02:46 +0900

    [Finish]

--- Desktop/js1/app.js
+++ Desktop/js1/app.js
@@ -1,8 +1,58 @@
-var i;
-var num = 0;
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand !="グー") && (user_hand !="チョキ") && (user_hand !="パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力してください');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+var js_hand = getJShand();
+var judge = winLose(user_hand, js_hand);
 
-for(i= 1;i<11;i++){
-  num = num + i;
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は'+ js_hand + 'です。\n結果は'+ judge +'です。');
+}else {
+  alert('またチャレンジしてね')
+}
+function getJShand(){
+  var js_hand_num = Math.floor(Math.random()*3);
+  var hand;
+
+if(js_hand_num == 0){
+  hand ="グー";
+} else if(js_hand_num == 1){
+  hand = "チョキ";
+} else if(js_hand_num == 2){
+  hand = "パー";
+}
+return hand;
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+function winLose(user,js){
+  var winLoseStr;
+
+  if(user =="グー"){
+    if(js =="グー"){
+      winLoseStr ="あいこ";
+    } else if(js =="チョキ"){
+      winLoseStr ="勝ち";
+    } else if(js =="パー"){
+      winLoseStr ="負け";
+    }
+  } else if(user =="チョキ"){
+    if(js =="グー"){
+      winLoseStr ="負け";
+    } else if(js =="チョキ"){
+      winLoseStr ="あいこ";
+    } else if(js =="パー"){
+      winLoseStr ="勝ち";
+    }
+  } else if(user =="パー"){
+    if(js =="グー"){
+      winLoseStr ="勝ち";
+    } else if(js =="チョキ"){
+      winLoseStr ="負け";
+    } else if(js =="パー"){
+      winLoseStr ="あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
